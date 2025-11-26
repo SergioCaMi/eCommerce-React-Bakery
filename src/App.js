dc0e87ec49e0ab7124ead37e910f9e52cc485660
Sergio Calvo <sergiocami84@gmail.com>
2025-11-26 19:58:39 +0100

    Add category item component and styles
    - Created a new CategoryItem component to display individual categories with an image and title.
    - Added styles for the category item to enhance layout and hover effects.
    - Introduced a categories container style to manage the overall layout of category items.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,83 @@
-
+import "./categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component";
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Bollería Dulce",
+      img: "./images/dulce.png",
+    },
+    {
+      id: 2,
+      title: "Bollería Salada",
+      img: "./images/salada.png",
+    },
+    {
+      id: 3,
+      title: "Panadería Industrial",
+      img: "./images/panaderia.png",
+    },
+    {
+      id: 4,
+      title: "Productos de Temporada",
+      img: "./images/temporada.png",
+    },
+    {
+      id: 5,
+      title: "Productos Especiales",
+      img: "./images/especiales.png",
+    },
+  ];
   return (
-    <div className="App">
-      Hello World!
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={ category } />
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// <div class="category-container">
+//   {/* <img></img> */}
+//   <div className="category-body-container">
+//     <h2>Bollería Dulce</h2>
+//     <p>Comprar ahora!</p>
+//   </div>
+// </div>
+// <div class="category-container">
+//   {/* <img></img> */}
+//   <div className="category-body-container">
+//     <h2>Bollería Salada</h2>
+//     <p>Comprar ahora!</p>
+//   </div>
+// </div>
+// <div class="category-container">
+//   {/* <img></img> */}
+//   <div className="category-body-container">
+//     <h2>Panadería Industrial</h2>
+//     <p>Comprar ahora!</p>
+//   </div>
+// </div>
+// <div class="category-container">
+//   {/* <img></img> */}
+//   <div className="category-body-container">
+//     <h2>Productos de Temporada</h2>
+//     <p>Comprar ahora!</p>
+//   </div>
+// </div>
+// <div class="category-container">
+//   {/* <img></img> */}
+//   <div className="category-body-container">
+//     <h2>Snacks y Mini Bollería</h2>
+//     <p>Comprar ahora!</p>
+//   </div>
+// </div>
+// <div class="category-container">
+//   {/* <img></img> */}
+//   <div className="category-body-container">
+//     <h2>Productos Especiales</h2>
+//     <p>Comprar ahora!</p>
+//   </div>
+// </div>
---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { img, title } = category;
+  return (
+    <div className="category-container">
+      {/* <img src={img} alt={title}></img> */}
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Comprar ahora!</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
